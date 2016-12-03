66fc3b97b90f0ea32caf064059c41f5ed88ba4bb
ANDRES <ANDRES@10.1.18.204>
2016-12-03 16:27:27 -0500

    Creación de Clase metodos

--- src/calle13/Calle13.java
+++ src/calle13/Calle13.java
@@ -23,7 +23,13 @@ public class Calle13 {
         Televisor tv1=new Televisor(35.0, 65.5, "LG","120VAC");//Instancia del televisor
 //        
 int opcion;
-        do {
+System.out.println("Seleccione una opcion");
+tv1.mostrarMenuElectro();
+opcion = read.nextInt();
+    switch  (opcion){
+                
+                case 1:
+                    do {
             System.out.println("Ingrese 1 para encender el televisor");
             opcion = read.nextInt();
             if (opcion==1) {
@@ -70,6 +76,38 @@ int opcion;
             }
             System.out.println(tv1);
         } while (opcion!=1);
+        
+                    break;
+                    
+                case 2:
+                    tv1.subirVol();
+                    break;
+                    
+                case 3:
+                    tv1.bajarVolumen();
+                    break;
+                    
+                case 4:
+                    tv1.subirCanal();
+                    break;
+                    
+                case 5:
+                    tv1.bajarCanal();
+                    break;
+                    
+                case 6:
+                    tv1.silenciar();
+                    break;
+                    
+                case 7:
+                    System.out.println("Ingrese el canal");
+                    tv1.cambiarCanal(read.nextInt());
+                    break;              
+            }
+
+
+
+
         
         
         
