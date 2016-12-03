d6e7d3afa676b852444413c513a528696f84a5f0
ANDRES <ANDRES@10.1.18.204>
2016-12-03 15:35:10 -0500

    Creación de Clase

---
+++ src/calle13/Grabadora.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calle13;
+
+/**
+ *
+ * @author ANDRES
+ */
+public class Grabadora extends Electrodomestico
+{
+    boolean encendido;
+    int volumen;
+    int voltemp;
+    boolean mute;
+    double frecuencia;
+    boolean modulacion;
+    boolean unidad;
+    boolean reproduccion;
+    boolean pausada;
+    int cancion;
+    boolean bandeja;
+
+    public Grabadora(String marca, String entradaelectrica) {
+        super(marca, entradaelectrica);
+    }
+    
+    
+    @Override
+    void enceder() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+       
+}
